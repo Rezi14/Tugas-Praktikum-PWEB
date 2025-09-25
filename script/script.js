447eaa0f329ddee461ecd744be5a5da6989b3a42
Rezikecap <fahrezianggalesmana14@gmail.com>
2025-09-25 23:03:08 +0700

    Tugas Praktikum PWEB

--- script/script.js
+++ script/script.js
@@ -1,8 +1,20 @@
-// Ambil navbar dari layout.html dan masukkan ke setiap halaman
+function highlightActiveLink() {
+  const currentPath = window.location.pathname;
+  const navLinks = document.querySelectorAll(".navbar a");
+
+  navLinks.forEach(link => {
+    const linkPath = link.getAttribute('href');
+    if (currentPath.endsWith(linkPath)) {
+      link.classList.add('active');
+    }
+  });
+}
+
 fetch("/Layout/navbar.html")
   .then(response => response.text())
   .then(data => {
     document.querySelector(".navbar").innerHTML = data;
+    highlightActiveLink()
   });
 
   fetch("/Layout/footer.html")
